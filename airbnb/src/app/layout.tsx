42e63ce2ecae307763d1eedff2b31af946499178
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-26 16:59:56 +0900

    Chaged folder name NavBar to navbar

--- airbnb/src/app/layout.tsx
+++ airbnb/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 
 export const metadata: Metadata = {
