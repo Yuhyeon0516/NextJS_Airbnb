ba09fac04a0f6c8cbd65e20e3bde4e1686bd3981
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-27 20:47:19 +0900

    Created ListingCategory component.

--- airbnb/src/app/actions/getListingById.ts
+++ airbnb/src/app/actions/getListingById.ts
@@ -4,8 +4,9 @@ interface IParams {
     listingId?: string;
 }
 
-export default async function getListingById({ listingId }: IParams) {
+export default async function getListingById(params: IParams) {
     try {
+        const { listingId } = params;
         const listing = await prisma.listing.findUnique({
             where: {
                 id: listingId,
