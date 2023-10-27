9cec339fc52a66e1d586c6639b7be530a1be1cb2
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-27 13:50:26 +0900

    Created function that register modal toggling.

--- airbnb/src/app/components/modals/RegisterModal.tsx
+++ airbnb/src/app/components/modals/RegisterModal.tsx
@@ -12,9 +12,11 @@ import Input from "../inputs/Input";
 import toast from "react-hot-toast";
 import Button from "../Button";
 import { signIn } from "next-auth/react";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 export default function RegisterModal() {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -38,6 +40,11 @@ export default function RegisterModal() {
             .catch((error) => toast.error(error.message))
             .finally(() => setIsLoading(false));
     }
+
+    const toggleLogin = useCallback(() => {
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [loginModal, registerModal]);
 
     const bodyContent = (
         <div className=" flex flex-col gap-4">
@@ -89,7 +96,7 @@ export default function RegisterModal() {
                 <div className=" flex flex-row items-center justify-center gap-2">
                     <div>Already have an account?</div>
                     <div
-                        onClick={registerModal.onClose}
+                        onClick={toggleLogin}
                         className=" text-neutral-800 cursor-pointer hover:underline"
                     >
                         Log in
